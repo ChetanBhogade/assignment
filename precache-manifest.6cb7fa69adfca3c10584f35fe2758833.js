540d93a176c0edaa66e1d26dc4bde1548ee3aedd
Chetan Bhogade <chetan.bhogade3899@gmail.com>
2020-08-24 09:30:27 +0530

    Updates

---
+++ precache-manifest.6cb7fa69adfca3c10584f35fe2758833.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d11c21f26ef3242966ad19e04abf055",
+    "url": "/assignment/index.html"
+  },
+  {
+    "revision": "f924a46107b1bfc1eb5e",
+    "url": "/assignment/static/css/main.46b5622a.chunk.css"
+  },
+  {
+    "revision": "80d22e92f35f8e2dc816",
+    "url": "/assignment/static/js/2.90116255.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/assignment/static/js/2.90116255.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f924a46107b1bfc1eb5e",
+    "url": "/assignment/static/js/main.e2ee0aed.chunk.js"
+  },
+  {
+    "revision": "27444e82d0e0dc18426b",
+    "url": "/assignment/static/js/runtime-main.acb3cd1d.js"
+  },
+  {
+    "revision": "6daec7c06ddc26a1e675bedcce1488f7",
+    "url": "/assignment/static/media/second-img.6daec7c0.jpg"
+  }
+]);
